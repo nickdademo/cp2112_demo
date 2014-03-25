7ada405962a79fef82367f339bea3e3149d74b2d
Nick D'Ademo <nickdademo@gmail.com>
2014-03-26 01:53:18 +1100

    Add enum to hold SBS command addresses. Add extra demo reads.

--- cp2112_demo/cp2112_demo.cpp
+++ cp2112_demo/cp2112_demo.cpp
@@ -40,8 +40,8 @@ int main(int argc, char* argv[])
         fprintf(stderr,"Device successfully configured.\r\n");
     }
     
-    // Read Pack Voltage [0x09]
-    if(SMBus_Read(&m_hidSmbus, buffer, 0x16, 0x09, 2) != 0)
+    // Read Voltage [0x09]
+    if(SMBus_Read(&m_hidSmbus, buffer, SLAVE_WRITE_ADDRESS, VOLTAGE, 2) != 0)
     {
         fprintf(stderr,"ERROR: Could not perform SMBus read.\r\n");
         SMBus_Close(&m_hidSmbus);
@@ -49,11 +49,64 @@ int main(int argc, char* argv[])
     }
     else
     {
-        unsigned int packVoltage_mV = (buffer[1] << 8) | buffer[0];
-        fprintf(stderr, "Pack Voltage = %d mV\r\n", packVoltage_mV);
+        UINT16 voltage_mV = (buffer[1] << 8) | buffer[0];
+        fprintf(stderr, "Voltage = %d mV\r\n", voltage_mV);
+    }
+
+	// Read Current [0x0A]
+    if(SMBus_Read(&m_hidSmbus, buffer, SLAVE_WRITE_ADDRESS, CURRENT, 2) != 0)
+    {
+        fprintf(stderr,"ERROR: Could not perform SMBus read.\r\n");
+        SMBus_Close(&m_hidSmbus);
+        return -1;
+    }
+    else
+    {
+        INT16 current_mA = (buffer[1] << 8) | buffer[0];
+        fprintf(stderr, "Current = %d mA\r\n", current_mA);
+    }
+
+	// Relative State of Charge [0x0D]
+    if(SMBus_Read(&m_hidSmbus, buffer, SLAVE_WRITE_ADDRESS, RELATIVE_STATE_OF_CHARGE, 1) != 0)
+    {
+        fprintf(stderr,"ERROR: Could not perform SMBus read.\r\n");
+        SMBus_Close(&m_hidSmbus);
+        return -1;
+    }
+    else
+    {
+        UINT16 rsoc = (buffer[1] << 8) | buffer[0];
+        fprintf(stderr, "RSOC = %d %%\r\n", rsoc);
+    }
+
+	// Remaining Capacity [0x0F]
+    if(SMBus_Read(&m_hidSmbus, buffer, SLAVE_WRITE_ADDRESS, REMAINING_CAPACITY, 2) != 0)
+    {
+        fprintf(stderr,"ERROR: Could not perform SMBus read.\r\n");
+        SMBus_Close(&m_hidSmbus);
+        return -1;
+    }
+    else
+    {
+        UINT16 remCap = (buffer[1] << 8) | buffer[0];
+        fprintf(stderr, "Remaining Capacity = %d mAh\r\n", remCap);
+    }
+
+	// Average Time to Empty [0x12]
+    if(SMBus_Read(&m_hidSmbus, buffer, SLAVE_WRITE_ADDRESS, AVERAGE_TIME_TO_EMPTY, 2) != 0)
+    {
+        fprintf(stderr,"ERROR: Could not perform SMBus read.\r\n");
+        SMBus_Close(&m_hidSmbus);
+        return -1;
+    }
+    else
+    {
+        UINT16 avgTimeToEmpty = (buffer[1] << 8) | buffer[0];
+        fprintf(stderr, "Average Time to Empty = %d min(s)\r\n", avgTimeToEmpty);
     }
 
     // Success
+	fprintf(stderr, "Done! Exiting...\r\n");
     SMBus_Close(&m_hidSmbus);
     return 0;
 }
